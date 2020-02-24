850d3777e460e4e1344d830c3f1858373847cf86
CycoBlaze <61288200+CycoBlaze@users.noreply.github.com>
2020-02-24 16:28:30 -0500

    Create Customer.cpp

---
+++ Customer.cpp
@@ -0,0 +1,71 @@
+public:
+    Customer::Customer()
+    {
+    std::sting customerNum="";
+    std::string customerName="";
+    double lineOfCredit=0;
+    corperateAddress="";
+    }
+
+    Customer::Customer(std::sting c_num, std::string c_name, double c_credit)
+    {
+    customerNum = c_num;
+    customerName = c_name;
+    lineOfCredit = c_credit;
+    }
+    
+    Customer::Customer()
+    
+    std::string getCustomerNum(){return customerNum;}
+    std::string getCustomerName(){return customerName;}
+    std::double getLineOfCredit(){return lineOfCredit;}
+
+    void std::string setCustomerNum(){return customerNum;}
+    void std::string setCustomerName(){return customerName;}
+    void std::double setLineOfCredit(){return lineOfCredit;}
+
+    //Receive from file functions
+	void Customer::grabcustomer()
+	{
+        int lineOfCredit;
+        std::string customerNum; 
+        std::string customerName; 
+        std::string street; 
+        std::string city; 
+        std::string state; 
+        std::string zip;
+        char c;
+        vector<Customer> customers;
+        vector<Address> addresses;
+
+        ifstream inFile;
+        inFile.open("customers.dat");
+        if(inFile.is_open())
+        {
+            while (!inFile.eof())
+            {
+                getline(inFile, customerNum, '|');
+                getline(inFile, customerName, '|');
+                inFile >> lineOfCredit >> c;
+                getline(inFile, street, ',');
+                getline(inFile, city, ',');
+                getline(inFile, state, ',');
+                getline(inFile, zip);
+                Customer one(customerNum, customerName, lineOfCredit);
+                Address two(street, city, state, zip);
+                customers.push_back(one);
+                addresses.push_back(two);      
+            }
+        }
+	        inFile.close();
+	    arrCustomer=customers;
+	    arrAddress=addresses;
+	}
+
+
+
+    std::string Customer::getCustomer()
+	{
+	    std::string lineOfCredit= (std::string string) lineOfCredit;
+	    return customerNum+customerName+lineOfCredit;
+	}
